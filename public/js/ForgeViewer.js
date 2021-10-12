43aedf0b43f6b48111383bdc4d15e97f95359010
hedinang <tridunga1@gmail.com>
2021-10-13 00:08:19 +0700

    update

---
+++ public/js/ForgeViewer.js
@@ -0,0 +1,73 @@
+﻿/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+var viewer;
+
+function launchViewer(urn) {
+  var options = {
+    env: 'AutodeskProduction',
+    getAccessToken: getForgeToken
+  }
+
+  Autodesk.Viewing.Initializer(options, () => {
+    viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'))
+    viewer.start()
+    var documentId = 'urn:' + urn
+    Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure)
+  })
+}
+
+let hexToRgb = hex =>
+  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+    , (m, r, g, b) => '#' + r + r + g + g + b + b)
+    .substring(1).match(/.{2}/g)
+    .map(x => parseInt(x, 16))
+
+function onDocumentLoadSuccess(doc) {
+  var viewables = doc.getRoot().getDefaultGeometry();
+  viewer.loadDocumentNode(doc, viewables).then(i => {
+    // documented loaded, any action?
+    $.ajax({
+      type: 'GET',
+      url: '/api/forge/oss/color',
+      success: function (data) {
+        data.forEach(e => {
+          let color = hexToRgb(e.color)
+          let colorVector = new THREE.Vector4(color[0] / 255, color[1] / 255, color[2] / 255, 1)
+          viewer.setThemingColor(e.id, colorVector)
+        });
+        console.log('aa');
+      },
+      error: function (data) {
+        alert("Server has an error")
+      }
+    })
+  })
+}
+
+function onDocumentLoadFailure(viewerErrorCode) {
+  console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode)
+}
+
+function getForgeToken(callback) {
+  fetch('/api/forge/oauth/token').then(res => {
+    res.json().then(data => {
+      callback(data.access_token, data.expires_in)
+    })
+  })
+}
